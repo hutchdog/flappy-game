6c03593632b72fa0d8bb26004d51a75f15ddd143
Dmitry Shabanov <shabanovdmitry@gmail.com>
2021-06-27 04:11:27 +0300

    Default fullscreen Java app.

---
+++ app/src/main/java/com/example/angrygeom/FullscreenActivity.java
@@ -0,0 +1,26 @@
+package com.example.angrygeom;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FullscreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_fullscreen);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+    }
+}

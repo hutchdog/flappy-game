d3b2dce9ad9ec15e919a8751b98f99f9629d4c3a
Dmitry Shabanov <shabanovdmitry@gmail.com>
2021-06-27 05:31:56 +0300

    Minimal JNI test.

---
+++ app/game/main.cpp
@@ -0,0 +1,10 @@
+#include <jni.h>
+
+extern "C" {
+
+JNIEXPORT jint JNICALL
+Java_com_example_angrygeom_GameView_test(JNIEnv *env, jobject thiz) {
+    return 0;
+}
+
+}
--- app/src/main/java/com/example/angrygeom/GameView.java
+++ app/src/main/java/com/example/angrygeom/GameView.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.opengl.GLSurfaceView;
 
 public class GameView extends GLSurfaceView  {
+    static {
+        System.loadLibrary("game");
+    }
+
     OpenGLRenderer m_openGLRenderer;
 
     public GameView(Context context) {
@@ -12,5 +16,9 @@ public class GameView extends GLSurfaceView  {
         m_openGLRenderer = new OpenGLRenderer();
         setEGLContextClientVersion(2);
         setRenderer(m_openGLRenderer);
+
+        test();
     }
+
+    public native int test();
 }
